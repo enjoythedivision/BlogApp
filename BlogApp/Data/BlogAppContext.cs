daffebc149d8c6855ad81171a4db149ec96c7867
Maria <147405522+enjoythedivision@users.noreply.github.com>
2026-02-14 22:43:36 +0200

    Add project files.

---
+++ BlogApp/Data/BlogAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlogApp.Models;
+
+namespace BlogApp.Data
+{
+    public class BlogAppContext : DbContext
+    {
+        public BlogAppContext (DbContextOptions<BlogAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogApp.Models.Post> Post { get; set; } = default!;
+    }
+}
